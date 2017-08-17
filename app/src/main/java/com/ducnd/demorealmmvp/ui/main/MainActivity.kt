01604af1d3e739d2526ebffde5746b28b6b806e8
Hoang Hiep <hoanghiep.hui@gmail.com>
2017-08-17 21:38:36 +0700

    using kotlin extensions

--- app/src/main/java/com/ducnd/demorealmmvp/ui/main/MainActivity.kt
+++ app/src/main/java/com/ducnd/demorealmmvp/ui/main/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.ducnd.demorealmmvp.ui.main
 
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.text.Editable
 import android.text.TextWatcher
-import android.view.View
-import android.widget.EditText
 import com.ducnd.demorealmmvp.R
-import com.ducnd.demorealmmvp.common.ExApplication
 import com.ducnd.demorealmmvp.remote.database.StoreManager
 import com.ducnd.demorealmmvp.remote.model.ItemSong
 import com.ducnd.demorealmmvp.remote.model.SongSearchResult
@@ -16,11 +12,10 @@ import com.ducnd.realmmvp.ui.customview.ImageViewLocal
 import com.ducnd.realmmvp.utils.action.Action1
 import io.reactivex.disposables.Disposable
 import io.realm.RealmList
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseMvpActivityM<IMain.Presenter>(), IMain.View, TextWatcher, SongAdapter.ISongAdapter {
     private var dispose: Disposable? = null
-    private var edtName: EditText? = null
-    private var rcSong: RecyclerView? = null
     private lateinit var mAdapter: SongAdapter
     private var mItemSong: MutableList<ItemSong>? = null
 
@@ -28,10 +23,7 @@ class MainActivity : BaseMvpActivityM<IMain.Presenter>(), IMain.View, TextWatche
         get() = R.layout.activity_main
 
     override fun findViewByIds() {
-        edtName = findViewById<View>(R.id.edt_name) as EditText
-        rcSong = findViewById<View>(R.id.rc_song) as RecyclerView
-
-        edtName!!.addTextChangedListener(this)
+        edt_name.addTextChangedListener(this)
     }
 
     override fun setEvents() {
@@ -39,9 +31,9 @@ class MainActivity : BaseMvpActivityM<IMain.Presenter>(), IMain.View, TextWatche
 
     override fun initComponents() {
         mAdapter = SongAdapter(this)
-        val manager: LinearLayoutManager = LinearLayoutManager(this);
-        rcSong!!.layoutManager = manager
-        rcSong!!.adapter = mAdapter
+        val manager = LinearLayoutManager(this);
+        rc_song.layoutManager = manager
+        rc_song.adapter = mAdapter
         mPresenter = MainPresenter(this, getAccountInteractor())
     }
 
@@ -49,7 +41,7 @@ class MainActivity : BaseMvpActivityM<IMain.Presenter>(), IMain.View, TextWatche
         if (dispose != null) {
             dispose!!.dispose()
         }
-        val name: String = edtName!!.text.toString().trim()
+        val name: String = edt_name.text.toString().trim()
         if (name == "") {
             mItemSong = null
             mAdapter.notifyDataSetChanged()
@@ -57,11 +49,11 @@ class MainActivity : BaseMvpActivityM<IMain.Presenter>(), IMain.View, TextWatche
         }
         dispose = mPresenter!!.getItemSong(name, object : Action1<MutableList<ItemSong>> {
             override fun call(data: MutableList<ItemSong>) {
-                val song: SongSearchResult = SongSearchResult()
+                val song = SongSearchResult()
                 song.itemSongs = RealmList<ItemSong>()
                 song.itemSongs!!.addAll(data.asIterable())
-                song.nameSearch = edtName!!.editableText.toString().trim()
-                song.id = edtName!!.editableText.toString().trim()
+                song.nameSearch = edt_name.editableText.toString().trim()
+                song.id = edt_name.editableText.toString().trim()
                 mPresenter!!.saveLocalSongResultSearch(song)
                 mItemSong = data
                 mAdapter.notifyDataSetChanged()
@@ -75,7 +67,7 @@ class MainActivity : BaseMvpActivityM<IMain.Presenter>(), IMain.View, TextWatche
 
     private fun errorSearch(error: Throwable) {
         if (error.message != null) {
-            val song: SongSearchResult? = mPresenter!!.getItemSongLocal(edtName!!.text.toString().trim())
+            val song: SongSearchResult? = mPresenter!!.getItemSongLocal(edt_name.text.toString().trim())
             if (song == null) {
                 mItemSong = null
             } else {
